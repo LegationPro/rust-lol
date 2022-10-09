2d09c3f4f0380e0c141cc62b94e9b9292e024fbe
Anze Peternel <peternelanze@gmail.com>
2022-10-09 14:36:16 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,14 @@ async fn html() -> Html<&'static str> {
     return Html(&test.as_str())
 }
 
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
 #[shuttle_service::main]
 async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/", get(html));
+    let router = Router::new().route("/", get(html)).route("/hi", get(hello_world));
     let sync_wrapper = SyncWrapper::new(router);
+
     Ok(sync_wrapper)
 }
