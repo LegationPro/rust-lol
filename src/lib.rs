ba466c50bb84e32e47ce063ab6d1ba482f949e4b
Anze Peternel <peternelanze@gmail.com>
2022-10-09 15:13:54 +0200

    initnit

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,9 @@
-use std::fs::File;
-use std::io::Read;
 use axum::response::Html;
 use axum::{routing::get, Router};
 use sync_wrapper::SyncWrapper;
 
 async fn html() -> Html<&'static str> {
-    let mut f = File::open("src/index.html").expect("not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("oh no");
-
-    let actual_str = String::from(&contents).to_string();
-    let test: &'static str = Box::leak(Box::new(actual_str));
-    return Html("<h1>hi nigger<h1>")
-
+    Html(include_str!("./index.html"))
 }
 
 async fn hello_world() -> &'static str {
